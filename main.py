63e2e6bdac5d4879d42c172ad8c691817ce6e5c7
guitar6029 <109701970+guitar6029@users.noreply.github.com>
2023-07-14 17:16:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import wordlist
+import hangman
 ## handman ascii stages art
 stages = ['''
   +---+
@@ -56,7 +58,7 @@ stages = ['''
       |
 =========
 ''']
-word_list = ["aardvark", "baboon", "camel"]
+word_list = wordlist.word_list
 tries = 6
 chosen_word = word_list[random.randint(0, len(word_list) - 1)] 
 
@@ -65,10 +67,14 @@ display = []
 for n in range(wordLength):
   display += '_'
 
+print(hangman.hangmanLogo)
 print(display)
 gameFinished = False
+previousGuesses = []
 
 while not gameFinished:
+  if len(previousGuesses) > 0 :
+    print(f'\n Previous guesses : {previousGuesses}')
   guess = input('Guess a letter, type here: ').lower()
   hasLetter = False
   print("\n")
@@ -78,20 +84,25 @@ while not gameFinished:
     else:
       print(f'You have {tries} more tries.')
   print(stages[tries])
+  if "_" not in display:
+    gameFinished = True
+    previousGuesses = []
+    print('You have won this round.')
+  
   if tries == 0:
     gameFinished = True
+    previousGuesses = []
     print("You have lost this round.")
   
   for n in range(len(chosen_word)):
     if chosen_word[n] == guess:
       display[n] = chosen_word[n]
+  previousGuesses.append(guess)
 
   if guess not in chosen_word:
       tries -= 1
 
-  if "_" not in display:
-    gameFinished = True
-    print('You have won this round.')
+  
 
   print("\n")
   print(*display, sep = ' ')
