58ef50631debec82b2edc2195300d91e6de08602
guitar6029 <109701970+guitar6029@users.noreply.github.com>
2023-07-14 16:17:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,98 @@
-#Step 1 
 import random
-
+## handman ascii stages art
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+tries = 6
 chosen_word = word_list[random.randint(0, len(word_list) - 1)] 
-print(chosen_word)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('Guess a letter, type here: ')
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-hasLetter = False
-for n in range(len(chosen_word)):
-  if chosen_word[n] == guess:
-    hasLetter = True
-  print(f'letter {chosen_word[n]}')
-print(f'has letter {hasLetter}')    
+
+wordLength = len(chosen_word)
+display = []
+for n in range(wordLength):
+  display += '_'
+
+print(display)
+gameFinished = False
+
+while not gameFinished:
+  guess = input('Guess a letter, type here: ').lower()
+  hasLetter = False
+  print("\n")
+  if tries != 0:
+    if tries == 1:
+      print(f'You have {tries} more try.')
+    else:
+      print(f'You have {tries} more tries.')
+  print(stages[tries])
+  if tries == 0:
+    gameFinished = True
+    print("You have lost this round.")
+  
+  for n in range(len(chosen_word)):
+    if chosen_word[n] == guess:
+      display[n] = chosen_word[n]
+
+  if guess not in chosen_word:
+      tries -= 1
+
+  if "_" not in display:
+    gameFinished = True
+    print('You have won this round.')
+
+  print("\n")
+  print(*display, sep = ' ')
+      
